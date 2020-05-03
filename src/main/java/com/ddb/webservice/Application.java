0a0747cb76e09e21abcd39d63d5d66ba59e24ebf
6toolplaybigman <jslimited21@gmail.com>
2020-05-03 16:59:08 +0900

    프로젝트 메인 클래스 작성  - @SpringBootApplication으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정  - @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야함  - main 메소드의 SpringApplication.run으로 내장 WAS 실행

---
+++ src/main/java/com/ddb/webservice/Application.java
@@ -0,0 +1,14 @@
+package com.ddb.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* Application 클래스는 메인 클래스
+* */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
