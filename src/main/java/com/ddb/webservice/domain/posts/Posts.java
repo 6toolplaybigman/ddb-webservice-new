6fd5516c310a678a72fb5a961c53ea7b35471e97
6toolplaybigman <jslimited21@gmail.com>
2020-05-09 23:33:33 +0900

    Posts 클래스 주석 추가  - 어노테이션 및 Posts 클래스에 대한 주석 추가

--- src/main/java/com/ddb/webservice/domain/posts/Posts.java
+++ src/main/java/com/ddb/webservice/domain/posts/Posts.java
@@ -6,6 +6,14 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+/*
+* 주요 어노테이션을 클래스에 가깝게.
+* 롬복은 코드를 단순화시켜 주지만 필수 어노테이션은 아님.
+* 주요 어노테이션인 @Entity를 클래스에 가깝게 두고, 롬복 어노테이션을 그 위로 둠.
+* Posts Class : 실제 DB의 테이블과 매칭될 클래스이며 보통 Entity 클래스라고도 함.
+* JPA를 사용하면 DB 데이터에 작업할 경우 실제 쿼리를 날리기보단, 이 Entity 클래스의 수정을 통해 작업함.
+* */
+
 @Getter
 @NoArgsConstructor
 @Entity
